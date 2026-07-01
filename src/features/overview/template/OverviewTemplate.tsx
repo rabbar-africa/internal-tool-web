e50f825cfa019fe817ec279ddb44bd978181d2c2
ayodeji <afuwapeayodeji2018@gmail.com>
2026-07-01 07:48:16 +0100

    feat: updated the dashboard

--- src/features/overview/template/OverviewTemplate.tsx
+++ src/features/overview/template/OverviewTemplate.tsx
@@ -1,13 +1,8 @@
-import { Head } from "@/components/seo/head";
 import { DashboardPage } from "../components/dashboard/DashboardPage";
 
 export function OverviewTemplate() {
   return (
     <>
-      <Head
-        title="Dashboard"
-        description="Financial overview for Rabbar Africa"
-      />
       <DashboardPage />
     </>
   );
